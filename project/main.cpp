2192541ed3f704821d81e0fbde2ceb47328cb813
Wilbur Tinsley <jr.tinsley@gtri.gatech.edu>
2018-08-02 20:55:04 -0400

    display sizes of various types

--- project/main.cpp
+++ project/main.cpp
@@ -1,8 +1,23 @@
 #include <QCoreApplication>
 
+#include <iostream>
+
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    std::cout << "Size of bool: " << sizeof(bool) << '\n';
+    std::cout << "Size of signed int: " << sizeof(signed int) << '\n';
+    std::cout << "Size of unsigned int: " << sizeof(unsigned int) << '\n';
+    std::cout << "Size of int: " << sizeof(int) << '\n';
+    std::cout << "Size of signed short: " << sizeof(signed short) << '\n';
+    std::cout << "Size of unsigned short: " << sizeof(unsigned short) << '\n';
+    std::cout << "Size of short: " << sizeof(short) << '\n';
+    std::cout << "Size of signed long: " << sizeof(signed long) << '\n';
+    std::cout << "Size of unsigned long: " << sizeof(unsigned long) << '\n';
+    std::cout << "Size of long: " << sizeof(long) << '\n';
+    std::cout << "Size of signed char: " << sizeof(signed char) << '\n';
+    std::cout << "Size of unsigned char: " << sizeof(unsigned char) << '\n';
+    std::cout << "Size of char: " << sizeof(char) << '\n';
+    std::cout << "Size of long long: " << sizeof(long long) << '\n';
 
-    return a.exec();
+    return 0;
 }
